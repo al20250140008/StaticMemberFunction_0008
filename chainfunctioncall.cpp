ec21413a5c35fb41290b651ac76f2752394e5a81
al20250140008 <alf80169@gmail.com>
2026-05-04 12:30:38 +0700

    menambahkan class buku beserta method setjudul dan getjudul

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+class buku{
+    string judul;
+    public:
+    buku setjudul(string judul){
+        this->judul = judul;
+        return *this;
+    }
+    string getjudul(){
+        return this->judul;
+    }
+};
