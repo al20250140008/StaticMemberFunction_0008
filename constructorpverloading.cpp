8eee710feea7c99e199cf13cdb78ca9c4e195bc7
al20250140008 <alf80169@gmail.com>
2026-05-04 11:44:50 +0700

    menambahkan deklarasi class mahasiswa dengan beberapa konstruktor

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -2,3 +2,15 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+    private:
+    int nim;
+    string name;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
